acfab4fd61ca1eae1e40d473212ab527f2b3fdf9
VovaDVA <vl82d@mail.ru>
2026-01-14 01:45:10 +0300

    update docker-compose.yml

--- client/app/src/main/java/com/example/us/api/ApiClient.kt
+++ client/app/src/main/java/com/example/us/api/ApiClient.kt
@@ -35,7 +35,7 @@ object ApiClient {
         }
     }
 
-    const val BASE_URL = "http://192.168.0.109:8080"
+    const val BASE_URL = "http://193.42.126.208:8080"
     const val API_URL = "$BASE_URL/api"
 
     // --- универсальный GET ---
